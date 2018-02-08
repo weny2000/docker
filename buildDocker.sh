897a7e7edae15491167a1991d93fcd61f94bc8fb
root <root@ip-172-31-17-104.ap-northeast-1.compute.internal>
2018-02-08 15:00:02 +0000

    modify dockerfile & docker-compose.yml

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,7 +1,3 @@
 #bin/bash
 docker-compose stop
-nohup docker-compose up hrmsDB >> docker.log &
 docker-compose build data >> docker.log
-docker-compose build php >> docker.log
-docker-compose build tomcat01 >> docker.log
-docker-compose build nginx >> docker.log
--- rmImages.sh
+++ rmImages.sh
@@ -1,3 +1,5 @@
 #bin/bash
 docker-compose stop
 docker-compose rm -f
+docker rm -f `docker ps -a | awk 'NR>1{print $1}'`
+docker rmi -f `docker images | awk 'NR>1{print $3}'`
